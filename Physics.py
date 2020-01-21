74ee16247280bebce44f0c826392169b80636b39
Android5613 <46581198+Android5613@users.noreply.github.com>
2020-01-21 16:36:12 -0700

    Added Updated Program files.
    Added the new program files including the autonomous mode selector. The comments on the code can be cleaned up later.

--- Physics.py
+++ Physics.py
@@ -8,12 +8,11 @@ class PhysicsEngine:
         self.drive = drivetrains.FourMotorDrivetrain()
 
     def update_sim(self, hal_data, now, tm_diff):
-        lr_motor = hal_data["pwm"][1]["value"]
-        rr_motor = hal_data["pwm"][2]["value"]
-        lf_motor = hal_data["pwm"][3]["value"]
-        rf_motor = hal_data["pwm"][4]["value"]
+        lr_motor = hal_data["pwm"][2]["value"]
+        rr_motor = hal_data["pwm"][4]["value"]
+        lf_motor = hal_data["pwm"][1]["value"]
+        rf_motor = hal_data["pwm"][3]["value"]
 
         speed, rotation = self.drive.get_vector(lr_motor, rr_motor, lf_motor, rf_motor)
         self.physics_controller.drive(speed, rotation, tm_diff)
-
 
