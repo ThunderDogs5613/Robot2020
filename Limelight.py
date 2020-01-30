a89be6006d40d4dcf045b9623a045275c8a4b672
Android5613 <46581198+Android5613@users.noreply.github.com>
2020-01-30 15:38:23 -0700

    Added experimental limelight code
    Added experimental limelight code in separate file.

---
+++ Limelight.py
@@ -0,0 +1,48 @@
+# Python file to store the unfinished limelight code.
+import wpilib
+import networktables
+import numpy
+
+float.table = networktables.NetworkTable.getTable("limelight")
+float.tx = float.table.getNumber("tx")
+float.ty = float.table.getNumber("ty")
+
+
+class estimateDistance(float.ty):
+    mountAngle = 20
+    targetAngle = 20 + float.ty
+    botHeight = 2
+    targetHeight = 10
+
+    d = ((targetHeight - botHeight) / numpy.tan(targetAngle))
+
+
+# Steering factor for changing the entire system in one go.
+f = 1
+
+# Controls the maximum motor output and the minimum command that can go to the motors
+controlConstant = -0.1 * f
+minCommand = -0.05 * f
+
+"""
+if self.DriveStick.getRawButton(2):
+    
+    wpilib.run(estimateDistance)
+    
+    float.headingError = float.tx
+    self.steerAdjust = 0*f
+
+    if float.tx > 1.0
+        self.steerAdjust = kp*float.headingError + minCommand
+    elif float.tx < -1.0
+        self.steerAdjust = kp*float.headingError - minCommand
+    
+    leftDrive = self.steerAdjust
+    rightDrive = -self.steerAdjust
+    
+    self.drive.tankDrive(
+    leftDrive,
+    rightDrive,
+    squareInputs=False
+    )
+"""
